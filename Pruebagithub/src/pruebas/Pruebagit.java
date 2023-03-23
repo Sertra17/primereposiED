f5f063e4f2a23421c52eb9498b787be04c71dec9
Sertra17 <mario.sanchez23@educa.madrid.org>
2023-03-23 20:43:52 +0100

    Esto es la nueva pruebgit en mi repositorio

---
+++ Pruebagithub/src/pruebas/Pruebagit.java
@@ -0,0 +1,17 @@
+package pruebas;
+
+public class Pruebagit {
+
+	
+int guacamayo;
+
+int tupu;
+
+	private void laconshatuya() {
+		// TODO Auto-generated method stub
+System.out.println("Huele a crico mano");
+	}
+	
+	
+	
+}
